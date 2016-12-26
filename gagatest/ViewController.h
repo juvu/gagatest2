aca94c571d42292ed455f77841d74245c8ff98de
gaga <88@jisumai.com>
2016-12-26 17:20:09 +0800

    2
    2

---
+++ gagatest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gagatest
+//
+//  Created by Dong on 2016/12/26.
+//  Copyright © 2016年 jjjj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ gagatest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  gagatest
+//
+//  Created by Dong on 2016/12/26.
+//  Copyright © 2016年 jjjj. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
